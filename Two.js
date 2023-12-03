726b64fcd0dbbbc1efbc2813198ca9bb2562fa44
Prashant Sugave <prashantsugave125@gmail.com>
2023-12-03 12:14:47 +0530

    filter function

--- Two.js
+++ Two.js
@@ -25,3 +25,33 @@ Array1.forEach((elements) => {
   // In forEach loop using if condition push elements in newArray[]
 });
 console.log(newArray);
+
+//new example
+
+const books = [
+  { title: "Book One", gerne: "Finction", publish: 1981, edition: 2004 },
+
+  { title: "Book Two", gerne: "Non-Finction", publish: 1992, edition: 2008 },
+
+  { title: "Book Three", gerne: "History", publish: 1999, edition: 2007 },
+
+  { title: "Book Four", gerne: "Non-Finction", publish: 1989, edition: 2010 },
+
+  { title: "Book Five", gerne: "Science", publish: 2009, edition: 2014 },
+
+  { title: "Book Six", gerne: "Finction", publish: 1987, edition: 2010 },
+
+  { title: "Book Seven", gerne: "History", publish: 1986, edition: 1996 },
+
+  { title: "Book Eight", gerne: "Science", publish: 2011, edition: 2016 },
+
+  { title: "Book One", gerne: "Non-Finction", publish: 1981, edition: 1989 },
+];
+
+let userBooks = books.filter((bk) => bk.gerne === "History");
+
+userBooks = books.filter((bk) => {
+  return bk.publish >= 1995 && bk.gerne === "History";
+});
+
+console.log(userBooks);
